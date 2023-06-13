aace611a13a09fea44b9ed7ac255c9c7ba9a8d62
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2023-06-13 18:34:44 -0300

    Update VendaRepositorio.java
    O comentário do Javadoc foi adicionado para descrever o propósito da interface VendaRepositorio e sua relação com as operações de acesso a dados relacionadas às vendas.

--- Back/produtos/src/main/java/br/com/api/produtos/repositorio/VendaRepositorio.java
+++ Back/produtos/src/main/java/br/com/api/produtos/repositorio/VendaRepositorio.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.api.produtos.modelo.VendaModelo;
 
+/**
+ * Interface de repositório para operações de acesso a dados relacionadas às vendas.
+ */
 public interface VendaRepositorio extends CrudRepository<VendaModelo, Long> {
 
 }
